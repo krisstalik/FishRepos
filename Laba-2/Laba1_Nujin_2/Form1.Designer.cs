035276e6447c2bc020782c87e133872542c717a2
krisstalik <144616997+krisstalik@users.noreply.github.com>
2025-05-28 13:15:07 +0300

    Add files via upload

---
+++ Laba-2/Laba1_Nujin_2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Laba1_Nujin_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            B_Close = new Button();
+            SuspendLayout();
+            // 
+            // B_Close
+            // 
+            B_Close.Location = new Point(-1, -1);
+            B_Close.Name = "B_Close";
+            B_Close.Size = new Size(75, 23);
+            B_Close.TabIndex = 0;
+            B_Close.Text = "Close Form";
+            B_Close.UseVisualStyleBackColor = true;
+            B_Close.Click += B_Close_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Red;
+            ClientSize = new Size(800, 450);
+            Controls.Add(B_Close);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button B_Close;
+    }
+}
